f90da1c7c28b98e1b769ea9831673a04293176e2
Dif71 <93062302+Dif71@users.noreply.github.com>
2022-02-14 07:05:33 +0700

    Create al.sh

---
+++ al.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/Dif71/r/main/ins.sh | bash
+cd /root/bv
+curl https://raw.githubusercontent.com/Dif71/r/main/go.sh | bash
+
+echo ""
+echo " == [Done] == "
+echo ""
+
