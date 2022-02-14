1c6c9cca1767b479dc0af95ac29a83aff451a6ef
Dif71 <93062302+Dif71@users.noreply.github.com>
2022-02-14 11:39:59 +0700

    Update go.sh

--- go.sh
+++ go.sh
@@ -11,11 +11,11 @@ IFS=', ' read -r -a prxe <<< "$prxs"
 tref=${#refe[@]}
 tprx=${#prxe[@]}
 
-if [ "$run" -eq "s" ];then
+if [ "$run" == "s" ];then
 	echo "python3.8 crawl.py -t $dom -r ${refe[0]} -p ${prxe[0]}"
 	# python3.8 crawl.py -t "$dom" -r "${refe[0]}" -p "${prxe[0]}"
 
-elif [ "$run" -eq "p" ]; then
+elif [ "$run" == "p" ]; then
 	cou=1
 	for prx in "${prxe[@]}"
 	do
