ad9000cb3b07f3cd298efddc858f0868c7d6b707
Dif71 <93062302+Dif71@users.noreply.github.com>
2022-02-14 12:43:49 +0700

    Update al.sh

--- al.sh
+++ al.sh
@@ -3,9 +3,9 @@
 dom="$1"
 run="$2"
 
-curl https://raw.githubusercontent.com/Dif71/r/main/ins.sh | bash
-cd /root/bv
-curl https://raw.githubusercontent.com/Dif71/r/main/go.sh | bash -s "$dom" "$run"
+#curl https://raw.githubusercontent.com/Dif71/r/main/ins.sh | bash
+#cd /root/bv
+#curl https://raw.githubusercontent.com/Dif71/r/main/go.sh | bash -s "$dom" "$run"
 
 echo ""
 #echo " == [Done] == "
